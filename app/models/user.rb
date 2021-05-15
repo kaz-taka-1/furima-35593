40267200babe85617b163aef0ee38b6c5e0b0a68
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-16 01:45:44 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,14 @@ class User < ApplicationRecord
   validates :lastname_kana, presence: true
   validates :birthday, presence: true
   # validates :encrypted_password, presence: true, length: {minimum: 6}
-  
-
-  
 
   with_options presence: true do
     # ひらがな、カタカナ、漢字のみ許可する
-    validates :firstname, :lastname,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :firstname, :lastname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
     # カタカナのみ許可する
-    validates :firstname_kana, :lastname_kana,format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Input half-width characters.' 
+    validates :firstname_kana, :lastname_kana,
+              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Input half-width characters.'
   end
-  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password              {password}
-    password_confirmation {password}
-    firstname             {Gimei.name.first}
-    lastname              {Gimei.name.last}
-    firstname_kana        {Gimei.name.first.katakana}
-    lastname_kana         {Gimei.name.last.katakana}
-    birthday              {Faker::Date.between(from: '1930-01-01', to: '2016-03-31')}
-
+    password              { password }
+    password_confirmation { password }
+    firstname             { Gimei.name.first }
+    lastname              { Gimei.name.last }
+    firstname_kana        { Gimei.name.first.katakana }
+    lastname_kana         { Gimei.name.last.katakana }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2016-03-31') }
   end
 end
