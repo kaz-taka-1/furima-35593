1115affa1f597433991b06a7bf784d9fe8438e45
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-16 02:44:55 +0900

    バリデーションの記述修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  validates :firstname_kana, presence: true
-  validates :lastname_kana, presence: true
-  validates :birthday, presence: true
-  # validates :encrypted_password, presence: true, length: {minimum: 6}
-
   with_options presence: true do
-    # ひらがな、カタカナ、漢字のみ許可する
-    validates :firstname, :lastname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    # カタカナのみ許可する
-    validates :firstname_kana, :lastname_kana,
-              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :nickname, presence: true
+    validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :birthday, presence: true
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Input half-width characters.'
   end
