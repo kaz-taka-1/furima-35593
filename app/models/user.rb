0e8b3e75dfe8d6403db23767e0657e0cc82a7a8f
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-16 01:36:58 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,18 @@ class User < ApplicationRecord
   validates :firstname_kana, presence: true
   validates :lastname_kana, presence: true
   validates :birthday, presence: true
+  # validates :encrypted_password, presence: true, length: {minimum: 6}
   
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
   
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :firstname
-    validates :lastname
+
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :firstname, :lastname,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    # カタカナのみ許可する
+    validates :firstname_kana, :lastname_kana,format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Input half-width characters.' 
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
-    validates :firstname_kana
-    validates :lastname_kana
-  end
+  
 end
