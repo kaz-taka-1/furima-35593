04fc085e6851ba090b224d559d50b2e2c42ed247
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-16 16:59:11 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,6 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Input half-width characters.'
   end
+  has_many :items
+  # has_many :purchases
 end
