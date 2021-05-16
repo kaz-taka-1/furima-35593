132064022ed22dc6ec20aa6524ba2f27ea062c71
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-16 16:55:01 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_093835) do
+ActiveRecord::Schema.define(version: 2021_05_12_033637) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 2021_05_12_093835) do
     t.integer "prefectures_id", null: false
     t.integer "delivery_days_id", null: false
     t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_05_12_093835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
