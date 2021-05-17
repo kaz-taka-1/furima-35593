e0e95da6843e287797c6e64f609fda16fa82e05a
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-17 15:08:04 +0900

    active hashのクラス名修正

--- app/models/delivery_days.rb
+++
@@ -1,6 +0,0 @@
-class Delivery_charge < ActiveHash::Base
-  self.data = [
-    {id: 0, name: '--'}, {id: 1, name: '1〜２日で発送'}, {id: 2, name: '２〜３日で発送'}, {id: 2, name: '４〜７日で発送'}
-    
-              ]
-end
