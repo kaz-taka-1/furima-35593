2374ff18d69e1039abc0f648773c455b82f175dd
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-17 12:48:09 +0900

    active hashでの発送までの日数の一覧

---
+++ app/models/delivery_days.rb
@@ -0,0 +1,6 @@
+class Delivery_charge < ActiveHash::Base
+  self.data = [
+    {id: 0, name: '--'}, {id: 1, name: '1〜２日で発送'}, {id: 2, name: '２〜３日で発送'}, {id: 2, name: '４〜７日で発送'}
+    
+              ]
+end
