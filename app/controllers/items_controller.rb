ebdbbf7f435a746cb035d4455771cbf2ccb9ed19
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-17 21:33:56 +0900

    クリエイトアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,18 @@ class ItemsController < ApplicationController
   def new
     @item =  Item.new
   end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:text,:category_id,:status_id,:delivery_charge_id,:prefectures_id, :delivery_days_id,:price, :image).merge(user_id: current_user.id)
+  end
 end
