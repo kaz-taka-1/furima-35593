95a29a9256a3d110275361470188da826c8c9ff3
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-17 18:24:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ApplicationRecord
-
-  validates :image, presence: true
-  
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefectures_id
+    validates :delivery_days_id
+    validates :price
+    validates :image
+  end
   belongs_to :user
   # has_one :purchase
   has_one_attached :image
