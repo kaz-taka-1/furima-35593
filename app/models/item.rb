acf765c23aac0d3a6a1d81f83f3c1c54abda6587
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-18 23:15:44 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,19 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
-    validates :category_id
-    validates :status_id
-    validates :delivery_charge_id
-    validates :prefectures_id
-    validates :delivery_days_id
-    validates_inclusion_of :price, in:300..9999999
     validates :image
+    with_options numericality:{ other_than: 0 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_charge_id
+      validates :prefectures_id
+      validates :delivery_days_id
+      end
+      validates :price,numericality: {only_integer: true, greater_than_or_equal_to:300,less_than_or_equal_to:9999999,message: "is out of setting range." }
+      validates :price_before_type_cast,format: {with: /\A[a-z0-9]+\z/,message: "is invalid. Input half-width characters."}
   end
+ 
+ 
   belongs_to :user
   # has_one :purchase
   has_one_attached :image
