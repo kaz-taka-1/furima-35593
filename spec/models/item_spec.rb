c5487fb06af9beae20082449428161725ae54177
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-19 12:29:17 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,25 +30,50 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
+    it 'カテゴリーの情報が0だと保存できないこと' do
+      @item.category_id = '0'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Category must be other than 0')
+    end
     it '商品の状態についての情報が空だと登録できないこと' do
       @item.status_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+    it '商品の状態についての情報が0だと保存できないこと' do
+      @item.status_id = '0'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Status must be other than 0')
     end
     it '配送料の負担についての情報が空だと登録できないこと' do
       @item.delivery_charge_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
     end
+    it '配送料の負担についての情報が0だと保存できないこと' do
+      @item.delivery_charge_id = '0'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Delivery charge must be other than 0')
+    end
     it '発送元の地域についての情報が空だと登録できないこと' do
       @item.prefectures_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefectures can't be blank")
     end
+    it '発送元の地域についての情報が0だと保存できないこと' do
+      @item.prefectures_id = '0'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Prefectures must be other than 0')
+    end
     it '販売価格についての情報が空だと登録できないこと' do
       @item.price = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it '販売価格は全角文字だと登録できないこと' do
+      @item.price = '５００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is out of setting range.')
     end
     it '販売価格は、¥300より安いと登録できないこと' do
       @item.price = '100'
@@ -57,6 +82,16 @@ RSpec.describe Item, type: :model do
     end
     it '販売価格は、¥9,999,999より高いと登録できないこと' do
       @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is out of setting range.')
+    end
+    it '販売価格は英数字混合では登録できないこと' do
+      @item.price = '500yen'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is out of setting range.')
+    end
+    it '販売価格は英字のみでは登録できないこと' do
+      @item.price = 'yen'
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is out of setting range.')
     end
