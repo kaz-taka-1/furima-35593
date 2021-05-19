4d54630d78b6ec0c8f452f795cd4da6e41595a9e
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-19 17:42:10 +0900

    ActiveHashのアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,4 +18,9 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one :purchase
   has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :deliverycharge
+  belongs_to :prefectures
+  belongs_to :deliverydays
 end
