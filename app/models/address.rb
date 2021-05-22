26af024246baff282ba4f0c0d8ff030fc0c2942d
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-22 20:49:46 +0900

    addressモデルのバリテーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code
+    validates :prefectures_id
+    validates :municipalities
+    validates :street_number
+    validates :phone_number
+  end
   belongs_to :purchase
 end
