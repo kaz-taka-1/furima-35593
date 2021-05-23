12767460b475266f87a4010a0fca494b77ed706b
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-23 13:31:13 +0900

    addressのバリデーションの消去

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,3 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code
-    validates :prefectures_id,numericality:{ other_than: 0 }
-    validates :municipalities
-    validates :street_number
-    validates :phone_number
-  end
   belongs_to :purchase
 end
