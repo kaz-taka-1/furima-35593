549ab6cd72d2b0a6fbe0936a65b92f13b599749b
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-24 15:54:48 +0900

    商品購入画面へ遷移できるユーザーの条件

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_gate
+
   def index
-    @item = Item.find(params[:item_id])
-    @purchase_address = PurchaseAddress.new
+      @purchase_address = PurchaseAddress.new
   end
+
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    binding.pry
     if @purchase_address.valid?
       pry_item
       @purchase_address.save
@@ -30,4 +32,12 @@ class PurchasesController < ApplicationController
       currency: 'jpy'
     )
   end
+
+  def set_gate
+    @item = Item.find(params[:item_id])
+    if Purchase.exists?(item_id:@item.id) || @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+  
 end
