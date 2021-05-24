87cf88f2722e9f32609f622d859fced7b4abfa48
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-24 20:03:55 +0900

    FactoryBotの追加

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :purchase_address do
     postal_code { '123-4567' }
     prefectures_id { Faker::Number.between(from: 1, to: 47) }
-    municipalities { '東京都' }
+    municipalities { '八王子市' }
     street_number { '1-1' }
     building_number { '東京ハイツ' }
-    phone_number { 12_345_678_901 }
+    phone_number { 12345678901 }
     token { 'tok_abcdefghijk00000000000000000' }
+    user_id{Faker::Number}
+    item_id{Faker::Number}
   end
 end
