2ae276ad1ac547abd26f0a5119108842022af213
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-24 15:18:52 +0900

    クレジットカード決済機能の追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,12 +5,12 @@ class PurchasesController < ApplicationController
   end
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
+    binding.pry
     if @purchase_address.valid?
+      pry_item
       @purchase_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:format])
-      @purchase_address = PurchaseAddress.new
       render :index
     end
   end
@@ -18,7 +18,16 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefectures_id, :municipalities, :street_number, :building_number, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+    @item = Item.find(params[:item_id])
+    params.require(:purchase_address).permit(:postal_code, :prefectures_id, :municipalities, :street_number, :building_number, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token],price: @item.price)
   end
 
+  def pry_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: purchase_params[:price],
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -7,13 +7,13 @@ class PurchaseAddress
     validates :prefectures_id,numericality:{ other_than: 0 }
     validates :municipalities
     validates :street_number
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/}
   end
 
-  # def save
-  #   purchase = Purchase.create( user_id: user_id,item_id:@item.id)
-  #   Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipalities: municipalities,street_number: street_number,building_number: building_number, phone_number: phone_number)
-  # end
+  def save
+    purchase = Purchase.create( user_id: user_id,item_id:item_id)
+    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipalities: municipalities,street_number: street_number,building_number: building_number, phone_number: phone_number,purchase_id:purchase.id)
+  end
 
 end
 
