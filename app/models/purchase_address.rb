bf5eb711f52002ddbbe067605e05f79d4405dfad
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-24 19:44:12 +0900

    user_id、item_idのバリデーションの追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -10,6 +10,8 @@ class PurchaseAddress
     validates :street_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :token
+    validates :user_id
+    validates :item_id
   end
 
   def save
